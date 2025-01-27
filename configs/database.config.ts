df8aeda5a45bd438dc07755b6517d30574e77c22
Nguyen Duong Trong Tin <tinht5667@gmail.com>
2025-01-27 11:18:58 +0700

    fix: update .env

--- configs/database.config.ts
+++ configs/database.config.ts
@@ -1,11 +1,11 @@
 import { Sequelize } from "sequelize";
 
 const sequelize = new Sequelize(
-  "tours",
-  "root",
-  "",
+  process.env.DB_NAME as string,
+  process.env.DB_USERNAME as string,
+  process.env.DB_PASSWORD as string,
   {
-    host: "localhost",
+    host: process.env.DB_HOST as string,
     dialect: "mysql"
   }
 );
